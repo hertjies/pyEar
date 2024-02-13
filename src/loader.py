32ef123612814580d3ccffbdde13eb6615230f89
hertjies <htolken@gmail.com>
2024-02-13 22:09:48 +1100

    WIP: refactored loader

--- src/loader.py
+++ src/loader.py
@@ -1,53 +1,25 @@
 """
-Module for loading and initialising instances of external program requirements from disk/memory
+Module for loading and initialising instances in a specific order of external program requirements
 ie: Configuration, Player data (scores), Assets (Sound, GFX)
 """
-
-from config import Config
+import logging
+from src.config import Config
 
 class Loader:
     """
     Main instance loader class
     """
 
+    config = None
+    pygame = None
+    scores = None
+    font = None
+    sound = None
+    graphics = None
+
     def __init__(self) -> None:
-        self.config = self.load_instance_config()
-        self.scores = self.load_instance_scores()
-        self.font = self.load_instance_font()
-        self.sound = self.load_instance_sound()
-        self.graphics = self.load_instance_graphics()
-
-
-    def load_instance_config(self) -> None:
-        """
-        Load a configuration instance
-        """
-        return True
-
-
-    def load_instance_scores(self) -> None:
-        """
-        Load a player score instance
-        """
-        return True
-
-
-    def load_instance_font(self) -> None:
-        """
-        Load a font instance
-        """
-        return True
-
-
-    def load_instance_sound(self) -> None:
-        """
-        Load a sound store instance
-        """
-        return True
-
-
-    def load_instance_graphics(self) -> None:
-        """
-        Load a graphics store instance
-        """
-        return True
+        if self.config is None:
+            self.config = Config()
+            logging.info("Loader: set config")
+        else:
+            logging.warning("Loader: config already set")
--- src/main.py
+++ src/main.py
@@ -1,1 +1,6 @@
+from src.loader import Loader
+
 print(f"Called filename: {__name__}")
+
+global_loader = Loader()
+print(global_loader.config.configuration)
